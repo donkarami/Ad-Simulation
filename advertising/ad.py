d051b2899ae591c41b3e573e53dabd2ca96841db
mohamadamin karami <mohamadaminkarami80@gmail.com>
2021-01-10 23:50:17 +0330

    Ad class added

---
+++ advertising/ad.py
@@ -0,0 +1,10 @@
+from base_model import BaseAdvertising
+
+
+class Ad(BaseAdvertising):
+    def __init__(self, id, title, img_url, link, advertiser):
+        super().__init__(id)
+        self.__title = title
+        self.__img_url = img_url
+        self.__link = link
+        self.__advertiser = advertiser
