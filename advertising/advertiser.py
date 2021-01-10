69508cd50d9537266ceb0bf29552ba0f0497b2ec
mohamadamin karami <mohamadaminkarami80@gmail.com>
2021-01-10 23:58:29 +0330

    add get_name and set_name method

--- advertising/advertiser.py
+++ advertising/advertiser.py
@@ -7,3 +7,9 @@ class Advertiser(BaseAdvertising):
     def __init__(self, id, name):
         super().__init__(id)
         self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
